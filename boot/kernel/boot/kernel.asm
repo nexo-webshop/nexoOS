000c9939a55e310141880ede14b70e453e3a19fa
linux-rewritten-kernels <feddewestrahotmail@icloud.com>
2026-03-26 20:23:57 +0100

    Update kernel.asm

--- boot/kernel/boot/kernel.asm
+++ boot/kernel/boot/kernel.asm
@@ -1,8 +1,46 @@
+bits 32
+global kernel_main
+
 kernel_main:
-    mov byte [0xB8000], 'N'
-    mov byte [0xB8001], 0x07
 
-hang:
-    jmp hang
+    cli                         ; 🔥 interrupts uit (geen IDT nog)
 
-; if you see an 'N': kernel works!
+; ----------------
+; SEGMENTS RESET (BELANGRIJK)
+; ----------------
+    mov ax, 0x10                ; data segment selector
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+; ----------------
+; STACK (VEILIG + ALIGNED)
+; ----------------
+    mov esp, 0x90000
+    and esp, 0xFFFFFFF0
+
+    cld                         ; string ops safety
+
+; ----------------
+; VGA TEXT MODE WRITE
+; ----------------
+    mov edi, 0xB8000            ; VGA buffer
+    mov al, 'N'
+    mov ah, 0x07                ; lichtgrijs op zwart
+    mov [edi], ax
+
+; ----------------
+; DEBUG: meerdere chars (optioneel)
+; ----------------
+    mov word [edi+2], 0x0745    ; 'E'
+    mov word [edi+4], 0x0758    ; 'X'
+    mov word [edi+6], 0x074F    ; 'O'
+
+; ----------------
+; HALT LOOP (CPU vriendelijk)
+; ----------------
+.hang:
+    hlt
+    jmp .hang
